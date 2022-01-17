a58c52a5b202140f0748e9bee75c20a10d5e170a
BrunoSantoni <brunosantoni98@gmail.com>
2022-01-17 19:24:49 -0300

    refactor: move mapping to MongoHelper

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -6,14 +6,8 @@ import { MongoHelper } from '../helpers/mongo-helper';
 export class AccountMongoRepository implements AddAccountRepository {
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts');
-    const result = await accountCollection.insertOne(accountData);
-    const { insertedId: accountId } = result;
+    const { insertedId } = await accountCollection.insertOne(accountData);
 
-    const createdAccount: AccountModel = {
-      id: String(accountId),
-      ...accountData,
-    };
-
-    return createdAccount;
+    return MongoHelper.map<AddAccountModel>(insertedId, accountData);
   }
 }
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { Collection, MongoClient } from 'mongodb';
+import { Collection, MongoClient, ObjectId } from 'mongodb';
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -13,5 +13,12 @@ export const MongoHelper = {
 
   getCollection(name: string): Collection {
     return this.client.db().collection(name);
+  },
+
+  map<T>(mongoId: ObjectId, collectionData: T): { id: string } & T {
+    return {
+      id: String(mongoId),
+      ...collectionData,
+    };
   },
 };
