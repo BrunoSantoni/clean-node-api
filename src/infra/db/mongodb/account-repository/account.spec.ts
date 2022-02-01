e581958bd36f110f8bad979cde0909d101a84ace
BrunoSantoni <brunosantoni98@gmail.com>
2022-01-31 21:09:22 -0300

    test: ensure AccountMongoRepository returns an account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -2,8 +2,11 @@
 Teste de integração
 */
 
+import { Collection } from 'mongodb';
 import { MongoHelper } from '../helpers/mongo-helper';
 import { AccountMongoRepository } from './account';
+
+let accountCollection: Collection;
 
 describe(('Account Mongo Repository'), () => {
   beforeAll(async () => {
@@ -12,7 +15,7 @@ describe(('Account Mongo Repository'), () => {
 
   beforeEach(async () => {
     // Limpando a tabela antes dos outros testes
-    const accountCollection = await MongoHelper.getCollection('accounts');
+    accountCollection = await MongoHelper.getCollection('accounts');
 
     // Se passar com um objeto vazio, deleta todos os registros
     await accountCollection.deleteMany({});
@@ -24,13 +27,31 @@ describe(('Account Mongo Repository'), () => {
 
   const makeSut = (): AccountMongoRepository => new AccountMongoRepository();
 
-  test('Should return an account on success', async () => {
+  test('Should return an account on add success', async () => {
     const sut = makeSut();
     const account = await sut.add({
       name: 'any_name',
       email: 'any_email@mail.com',
       password: 'any_password',
     });
+
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toBe('any_name');
+    expect(account.email).toBe('any_email@mail.com');
+    expect(account.password).toBe('any_password');
+  });
+
+  test('Should return an account on loadByEmail success', async () => {
+    const sut = makeSut();
+
+    await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    });
+
+    const account = await sut.loadByEmail('any_email@mail.com');
 
     expect(account).toBeTruthy();
     expect(account.id).toBeTruthy();
