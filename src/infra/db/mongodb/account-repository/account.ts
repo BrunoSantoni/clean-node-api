0e74a31298e9cc83e7fa96648984313825af6ec5
BrunoSantoni <brunosantoni98@gmail.com>
2022-01-31 21:09:13 -0300

    feat: ensure AccountMongoRepository returns an account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,13 +1,25 @@
 import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository';
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository';
 import { AccountModel } from '../../../../domain/models/account';
 import { AddAccountModel } from '../../../../domain/usecases/add-account';
 import { MongoHelper } from '../helpers/mongo-helper';
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts');
     const { insertedId } = await accountCollection.insertOne({ ...accountData });
 
     return MongoHelper.map<AddAccountModel>(insertedId, accountData);
+  }
+
+  async loadByEmail(email: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts');
+    const { _id: mongoId, ...accountInfo } = await accountCollection.findOne({ email }) || {};
+
+    if (!mongoId || !accountInfo) return null;
+
+    const convertedAccountInfo = accountInfo as Omit<AccountModel, 'id'>;
+
+    return MongoHelper.map<Omit<AccountModel, 'id'>>(mongoId, convertedAccountInfo);
   }
 }
