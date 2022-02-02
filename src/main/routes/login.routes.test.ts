8b6a28e2b0ebc89d365ddbd5d292460981eb0449
BrunoSantoni <brunosantoni98@gmail.com>
2022-02-02 19:45:10 -0300

    refactor: rename signup routes to login routes

--- src/main/routes/login.routes.test.ts
+++ src/main/routes/login.routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
 import { app } from '../config/app';
 
-describe('SignUp Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL);
   });
@@ -16,12 +16,14 @@ describe('SignUp Routes', () => {
     await accountCollection.deleteMany({});
   });
 
-  test('Should return an account on success', async () => {
-    await request(app).post('/api/signup').send({
-      name: 'Bruno Santoni',
-      email: 'bsantoni98@gmail.com',
-      password: '123',
-      passwordConfirmation: '123',
-    }).expect(200);
+  describe('POST /signup', () => {
+    test('Should return 200 on POST /signup success', async () => {
+      await request(app).post('/api/signup').send({
+        name: 'Bruno Santoni',
+        email: 'bsantoni98@gmail.com',
+        password: '123',
+        passwordConfirmation: '123',
+      }).expect(200);
+    });
   });
 });
