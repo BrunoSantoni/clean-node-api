07a7099d0d731b95bfa8e0e16ab9c06b19762182
BrunoSantoni <brunosantoni98@gmail.com>
2022-02-09 20:08:04 -0300

    refactor: move validation to its own layer

--- src/presentation/controllers/signup/signup-controller-protocols.ts
+++ src/presentation/controllers/signup/signup-controller-protocols.ts
@@ -1,7 +1,6 @@
 // Protocolos (tipos/interfaces) específicos do signup controller
 
 export * from '../../protocols';
-export * from '../../protocols/validation';
 export * from '../../../domain/usecases/add-account';
 export * from '../../../domain/usecases/authentication';
 export * from '../../../domain/models/account';
