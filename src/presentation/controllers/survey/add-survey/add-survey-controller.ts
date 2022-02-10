449f59c7ae0bb25597bcb3411f0e3dee3f7c31a3
BrunoSantoni <brunosantoni98@gmail.com>
2022-02-09 21:06:37 -0300

    feat: ensure AddSurveyController returns 204 on success

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,4 +1,4 @@
-import { badRequest, serverError } from '../../../helpers/http/http-helper';
+import { badRequest, noContent, serverError } from '../../../helpers/http/http-helper';
 import {
   AddSurvey, Controller, HttpRequest, HttpResponse, Validation,
 } from './add-survey-controller-protocols';
@@ -21,7 +21,7 @@ export class AddSurveyController implements Controller {
 
       await this.addSurvey.add({ question, answers });
 
-      return null;
+      return noContent();
     } catch (error) {
       return serverError(error);
     }
