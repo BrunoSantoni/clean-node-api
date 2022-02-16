399536c5b8f676e229c1dcf98cecb7731e2a6711
BrunoSantoni <brunosantoni98@gmail.com>
2022-02-16 19:52:53 -0300

    test: ensure AccountMongoRepository will not find an account if find account by token fails

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -122,5 +122,11 @@ describe(('Account Mongo Repository'), () => {
       expect(account.email).toBe(httpRequest.email);
       expect(account.password).toBe(httpRequest.password);
     });
+
+    test('Should return null if loadByToken fails', async () => {
+      const sut = makeSut();
+      const account = await sut.loadByToken('any_token');
+      expect(account).toBeFalsy();
+    });
   });
 });
