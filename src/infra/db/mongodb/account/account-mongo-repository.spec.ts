b70db74ce714d864bffc8baee753e89d7a99fe92
BrunoSantoni <brunosantoni98@gmail.com>
2022-02-16 19:49:10 -0300

    test: ensure AccountMongoRepository find an account by token without role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -4,9 +4,16 @@ Teste de integração
 
 import { Collection } from 'mongodb';
 import { MongoHelper } from '../helpers/mongo-helper';
+import { AddAccountModel } from '../../../../domain/usecases/add-account';
 import { AccountMongoRepository } from './account-mongo-repository';
 
 let accountCollection: Collection;
+
+const makeFakeRequest = (): AddAccountModel => ({
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'any_password',
+});
 
 describe(('Account Mongo Repository'), () => {
   beforeAll(async () => {
@@ -30,37 +37,30 @@ describe(('Account Mongo Repository'), () => {
   describe('add()', () => {
     test('Should return an account on add success', async () => {
       const sut = makeSut();
-      const account = await sut.add({
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'any_password',
-      });
+      const httpRequest = makeFakeRequest();
+      const account = await sut.add(httpRequest);
 
       expect(account).toBeTruthy();
       expect(account.id).toBeTruthy();
-      expect(account.name).toBe('any_name');
-      expect(account.email).toBe('any_email@mail.com');
-      expect(account.password).toBe('any_password');
+      expect(account.name).toBe(httpRequest.name);
+      expect(account.email).toBe(httpRequest.email);
+      expect(account.password).toBe(httpRequest.password);
     });
   });
 
   describe('loadByEmail()', () => {
     test('Should return an account on loadByEmail success', async () => {
       const sut = makeSut();
+      const httpRequest = makeFakeRequest();
+      await accountCollection.insertOne(httpRequest);
 
-      await accountCollection.insertOne({
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'any_password',
-      });
-
-      const account = await sut.loadByEmail('any_email@mail.com');
+      const account = await sut.loadByEmail(httpRequest.email);
 
       expect(account).toBeTruthy();
       expect(account.id).toBeTruthy();
-      expect(account.name).toBe('any_name');
-      expect(account.email).toBe('any_email@mail.com');
-      expect(account.password).toBe('any_password');
+      expect(account.name).toBe(httpRequest.name);
+      expect(account.email).toBe(httpRequest.email);
+      expect(account.password).toBe(httpRequest.password);
     });
 
     test('Should return null if loadByEmail fails', async () => {
@@ -76,11 +76,7 @@ describe(('Account Mongo Repository'), () => {
     test('Should update the account accessToken on updateAccessToken success', async () => {
       const sut = makeSut();
 
-      const { insertedId } = await accountCollection.insertOne({
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'any_password',
-      });
+      const { insertedId } = await accountCollection.insertOne(makeFakeRequest());
 
       await sut.updateAccessToken(String(insertedId), 'any_token');
 
@@ -88,6 +84,25 @@ describe(('Account Mongo Repository'), () => {
 
       expect(account).toBeTruthy();
       expect(account.accessToken).toBe('any_token');
+    });
+  });
+
+  describe('loadByToken()', () => {
+    test('Should return an account on loadByToken without role', async () => {
+      const sut = makeSut();
+      const httpRequest = makeFakeRequest();
+
+      await accountCollection.insertOne({
+        ...httpRequest,
+        accessToken: 'any_token',
+      });
+
+      const account = await sut.loadByToken('any_token');
+      expect(account).toBeTruthy();
+      expect(account.id).toBeTruthy();
+      expect(account.name).toBe(httpRequest.name);
+      expect(account.email).toBe(httpRequest.email);
+      expect(account.password).toBe(httpRequest.password);
     });
   });
 });
