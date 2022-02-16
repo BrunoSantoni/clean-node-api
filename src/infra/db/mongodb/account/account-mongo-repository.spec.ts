8d753f60c9ec152076c683988b4c47be0b4e343c
BrunoSantoni <brunosantoni98@gmail.com>
2022-02-15 21:52:06 -0300

    refactor: separate tests in describe blocks

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -27,61 +27,67 @@ describe(('Account Mongo Repository'), () => {
 
   const makeSut = (): AccountMongoRepository => new AccountMongoRepository();
 
-  test('Should return an account on add success', async () => {
-    const sut = makeSut();
-    const account = await sut.add({
-      name: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'any_password',
-    });
+  describe('add()', () => {
+    test('Should return an account on add success', async () => {
+      const sut = makeSut();
+      const account = await sut.add({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+      });
 
-    expect(account).toBeTruthy();
-    expect(account.id).toBeTruthy();
-    expect(account.name).toBe('any_name');
-    expect(account.email).toBe('any_email@mail.com');
-    expect(account.password).toBe('any_password');
+      expect(account).toBeTruthy();
+      expect(account.id).toBeTruthy();
+      expect(account.name).toBe('any_name');
+      expect(account.email).toBe('any_email@mail.com');
+      expect(account.password).toBe('any_password');
+    });
   });
 
-  test('Should return an account on loadByEmail success', async () => {
-    const sut = makeSut();
+  describe('loadByEmail()', () => {
+    test('Should return an account on loadByEmail success', async () => {
+      const sut = makeSut();
 
-    await accountCollection.insertOne({
-      name: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'any_password',
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+      });
+
+      const account = await sut.loadByEmail('any_email@mail.com');
+
+      expect(account).toBeTruthy();
+      expect(account.id).toBeTruthy();
+      expect(account.name).toBe('any_name');
+      expect(account.email).toBe('any_email@mail.com');
+      expect(account.password).toBe('any_password');
     });
 
-    const account = await sut.loadByEmail('any_email@mail.com');
+    test('Should return null if loadByEmail fails', async () => {
+      const sut = makeSut();
 
-    expect(account).toBeTruthy();
-    expect(account.id).toBeTruthy();
-    expect(account.name).toBe('any_name');
-    expect(account.email).toBe('any_email@mail.com');
-    expect(account.password).toBe('any_password');
-  });
+      const account = await sut.loadByEmail('any_email@mail.com');
 
-  test('Should return null if loadByEmail fails', async () => {
-    const sut = makeSut();
-
-    const account = await sut.loadByEmail('any_email@mail.com');
-
-    expect(account).toBeFalsy();
-  });
-
-  test('Should update the account accessToken on updateAccessToken success', async () => {
-    const sut = makeSut();
-
-    const { insertedId } = await accountCollection.insertOne({
-      name: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'any_password',
+      expect(account).toBeFalsy();
     });
+  });
 
-    await sut.updateAccessToken(String(insertedId), 'any_token');
+  describe('updateAccessToken()', () => {
+    test('Should update the account accessToken on updateAccessToken success', async () => {
+      const sut = makeSut();
 
-    const account = await accountCollection.findOne({ _id: insertedId });
+      const { insertedId } = await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+      });
 
-    expect(account).toBeTruthy();
-    expect(account.accessToken).toBe('any_token');
+      await sut.updateAccessToken(String(insertedId), 'any_token');
+
+      const account = await accountCollection.findOne({ _id: insertedId });
+
+      expect(account).toBeTruthy();
+      expect(account.accessToken).toBe('any_token');
+    });
   });
 });
