0150bdf4dd5728569d8986aeb7b68405dbd5de74
BrunoSantoni <brunosantoni98@gmail.com>
2022-02-16 19:49:04 -0300

    feat: ensure AccountMongoRepository find an account by token without role

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -2,12 +2,16 @@ import { ObjectId } from 'mongodb';
 import { AddAccountRepository } from '../../../../data/protocols/db/account/add-account-repository';
 import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/account/load-account-by-email-repository';
 import { UpdateAccessTokenRepository } from '../../../../data/protocols/db/account/update-access-token-repository';
+import { LoadAccountByTokenRepository } from '../../../../data/usecases/load-account-by-token/db-load-account-by-token-protocols';
 import { AccountModel } from '../../../../domain/models/account';
 import { AddAccountModel } from '../../../../domain/usecases/add-account';
 import { MongoHelper } from '../helpers/mongo-helper';
 
 export class AccountMongoRepository implements
-AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository {
+AddAccountRepository,
+LoadAccountByEmailRepository,
+UpdateAccessTokenRepository,
+LoadAccountByTokenRepository {
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts');
     const { insertedId } = await accountCollection.insertOne({ ...accountData });
@@ -33,5 +37,19 @@ AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository 
         accessToken: token,
       },
     });
+  }
+
+  async loadByToken(token: string, role?: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts');
+    const { _id: mongoId, ...accountInfo } = await accountCollection.findOne({
+      accessToken: token,
+      role,
+    }) || {};
+
+    if (!mongoId || !accountInfo) return null;
+
+    const convertedAccountInfo = accountInfo as Omit<AccountModel, 'id'>;
+
+    return MongoHelper.map<Omit<AccountModel, 'id'>>(mongoId, convertedAccountInfo);
   }
 }
