9a64dd9f8b3b61ea37ff7e30048234cdc54cf063
Bruno Santoni <brunosantoni98@gmail.com>
2022-03-07 20:44:49 -0300

    test: ensure SaveSurveyResultController returns 200 on success

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -1,6 +1,6 @@
 import MockDate from 'mockdate';
 import { InvalidParamError } from "@/presentation/errors";
-import { forbidden, serverError } from "@/presentation/helpers/http/http-helper";
+import { forbidden, serverError, success } from "@/presentation/helpers/http/http-helper";
 import { SaveSurveyResultController } from "./save-survey-result-controller";
 import {
   HttpRequest,
@@ -145,5 +145,13 @@ describe('SaveSurveyResult Controller', () => {
     const httpResponse = await sut.handle(makeFakeRequest());
 
     expect(httpResponse).toEqual(serverError(new Error()));
+  });
+
+  test('Should return 200 on success', async () => {
+    const { sut } = makeSut();
+
+    const httpResponse = await sut.handle(makeFakeRequest());
+
+    expect(httpResponse).toEqual(success(makeFakeSurveyResult()));
   });
 })
