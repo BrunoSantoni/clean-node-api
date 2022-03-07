31e611f6482c52c5ad238f3a8b01bfa3682a8693
Bruno Santoni <brunosantoni98@gmail.com>
2022-03-07 20:39:12 -0300

    feat: ensure SaveSurveyResultController calls SaveSurveyResult with correct values

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller-protocols.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller-protocols.ts
@@ -1,4 +1,6 @@
 export * from '@/presentation/protocols/controller';
 export * from '@/presentation/protocols/http';
 export * from '@/domain/usecases/survey/load-survey-by-id';
+export * from '@/domain/usecases/survey-result/save-survey-result';
+export * from '@/domain/models/survey-result';
 export * from '@/domain/models/survey';
--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,16 +1,18 @@
-import { Controller, HttpRequest, HttpResponse, LoadSurveyById } from "./save-survey-result-controller-protocols";
+import { Controller, HttpRequest, HttpResponse, LoadSurveyById, SaveSurveyResult } from "./save-survey-result-controller-protocols";
 import { InvalidParamError } from "@/presentation/errors";
 import { forbidden, serverError } from '@/presentation/helpers/http/http-helper';
 
 export class SaveSurveyResultController implements Controller {
   constructor(
     private readonly loadSurveyById: LoadSurveyById,
+    private readonly saveSurveyResult: SaveSurveyResult,
   ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
 
     try {
       const { surveyId } = httpRequest.params;
+      const { accountId } = httpRequest;
       const { answer } = httpRequest.body;
       const survey = await this.loadSurveyById.loadById(surveyId);
   
@@ -24,6 +26,13 @@ export class SaveSurveyResultController implements Controller {
       if(!isValidAnswer) {
         return forbidden(new InvalidParamError('answer'));
       }
+
+      await this.saveSurveyResult.save({
+        surveyId,
+        accountId,
+        answer,
+        date: new Date(),
+      })
 
     } catch(error) {
       return serverError(error);
