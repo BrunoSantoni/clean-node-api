cec3c938c707e5165550a06d5467d39377404982
Bruno Santoni <brunosantoni98@gmail.com>
2022-03-11 19:49:29 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description: 'API para realizar enquetes que demonstra o uso do Clean Architecture em NodeJS',
+    version: '1.0.0',
+  },
+};
