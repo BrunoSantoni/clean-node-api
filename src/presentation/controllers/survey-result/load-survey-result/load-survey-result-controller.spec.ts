6a99e00cab8f883295514975eddf8ad23d7e6b52
Bruno Santoni <brunosantoni98@gmail.com>
2022-03-18 20:04:55 -0300

    test: ensure LoadSurveyResultController calls LoadSurveyById with correct id

---
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -0,0 +1,36 @@
+import { mockLoadSurveyById } from '@/presentation/test';
+import { LoadSurveyResultController } from './load-survey-result-controller';
+import { HttpRequest, LoadSurveyById } from './load-survey-result-controller-protocols';
+
+type SutTypes = {
+  sut: LoadSurveyResultController,
+  loadSurveyByIdStub: LoadSurveyById,
+};
+
+const mockRequest = (): HttpRequest => ({
+  params: {
+    surveyId: 'any_survey_id',
+  },
+});
+
+const makeSut = (): SutTypes => {
+  const loadSurveyByIdStub = mockLoadSurveyById();
+  const sut = new LoadSurveyResultController(loadSurveyByIdStub);
+
+  return {
+    sut,
+    loadSurveyByIdStub,
+  };
+};
+
+describe('LoadSurveyResult Controller', () => {
+  test('Should call LoadSurveyById with correct value', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut();
+
+    const loadByIdSpy = jest.spyOn(loadSurveyByIdStub, 'loadById');
+
+    await sut.handle(mockRequest());
+
+    expect(loadByIdSpy).toHaveBeenCalledWith('any_survey_id');
+  });
+});
