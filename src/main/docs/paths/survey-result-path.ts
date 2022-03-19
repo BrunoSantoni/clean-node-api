80883810e71537f9d509bb611bb1412adc694de6
Bruno Santoni <brunosantoni98@gmail.com>
2022-03-18 21:49:38 -0300

    docs: add load survey result path

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -6,6 +6,7 @@ export const surveyResultPath = {
     // Qual tag ele pertence criada no arquivo base
     tags: ['Surveys'],
     summary: 'API to save survey results',
+    description: 'This route can only be called by authenticated users',
     parameters: [{
       description: 'Survey ID param',
       in: 'path',
@@ -25,6 +26,45 @@ export const surveyResultPath = {
         },
       },
     },
+    responses: {
+      200: {
+        description: 'Success',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult',
+            },
+          },
+        },
+      },
+      403: {
+        $ref: '#/components/forbiddenResponse',
+      },
+      404: {
+        $ref: '#/components/notFoundResponse',
+      },
+      500: {
+        $ref: '#/components/serverErrorResponse',
+      },
+    },
+  },
+  get: {
+    security: [{
+      apiKeyAuth: [],
+    }],
+    // Qual tag ele pertence criada no arquivo base
+    tags: ['Surveys'],
+    summary: 'API to load survey results',
+    description: 'This route can only be called by authenticated users',
+    parameters: [{
+      description: 'Survey ID param',
+      in: 'path',
+      name: 'surveyId',
+      required: true,
+      schema: {
+        type: 'string',
+      },
+    }],
     responses: {
       200: {
         description: 'Success',
