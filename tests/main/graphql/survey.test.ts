1b9bb640b9b2104cfbf0194914c4cdf93b414830
Bruno Santoni <brunosantoni98@gmail.com>
2022-03-27 18:54:09 -0300

    test: added tests to gql survey route

---
+++ tests/main/graphql/survey.test.ts
@@ -0,0 +1,118 @@
+import { Collection } from 'mongodb';
+import { sign } from 'jsonwebtoken';
+import request from 'supertest';
+import { app } from '@/main/config/app';
+import { env } from '@/main/config/env';
+import { MongoHelper } from '@/infra/db';
+
+let surveyCollection: Collection;
+let accountCollection: Collection;
+const mockAccessToken = async (): Promise<string> => {
+  const { insertedId } = await accountCollection.insertOne({
+    name: 'Bruno Santoni',
+    email: 'bsantoni98@gmail.com',
+    password: '123',
+    role: 'admin',
+  });
+
+  const id = insertedId.toHexString();
+  const accessToken = sign({ id }, env.jwtSecret);
+
+  await accountCollection.updateOne({
+    _id: insertedId,
+  }, {
+    $set: {
+      accessToken,
+    },
+  });
+  return accessToken;
+};
+
+describe('Survey GraphQL', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys');
+    accountCollection = await MongoHelper.getCollection('accounts');
+
+    await surveyCollection.deleteMany({});
+    await accountCollection.deleteMany({});
+  });
+
+  describe('Surveys Query', () => {
+    const query = `query {
+      surveys {
+        id
+        question
+        answers {
+          image
+          answer
+        }
+        date
+        didUserAnswered
+      }
+    }`;
+
+    test('Should return Surveys', async () => {
+      const accessToken = await mockAccessToken();
+      const now = new Date();
+
+      await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com',
+        }, {
+          answer: 'Answer 2',
+        }],
+        date: now,
+      });
+
+      const res = await request(app)
+        .post('/graphql')
+        .set('x-access-token', accessToken)
+        .send({ query });
+
+      expect(res.status).toBe(200);
+      expect(res.body.data.surveys.length).toBe(1);
+      expect(res.body.data.surveys[0].id).toBeTruthy();
+      expect(res.body.data.surveys[0].question).toBe('Question');
+      expect(res.body.data.surveys[0].date).toBe(now.toISOString());
+      expect(res.body.data.surveys[0].didUserAnswered).toBe(false);
+      expect(res.body.data.surveys[0].answers).toEqual([{
+        answer: 'Answer 1',
+        image: 'http://image-name.com',
+      }, {
+        answer: 'Answer 2',
+        image: null,
+      }]);
+    });
+
+    test('Should return AccessDeniedError if no token is provided', async () => {
+      await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com',
+        }, {
+          answer: 'Answer 2',
+        }],
+        date: new Date(),
+      });
+
+      const res = await request(app)
+        .post('/graphql')
+        .send({ query });
+
+      expect(res.status).toBe(403);
+      expect(res.body.data).toBeFalsy();
+      expect(res.body.errors[0].message).toBe('Access Denied');
+    });
+  });
+});
